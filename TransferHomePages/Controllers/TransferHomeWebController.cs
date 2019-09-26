41e1f594887b5727a37e0a607f5dfbf22979a5c3
Tayyab <tthunjra@gmail.com>
2019-09-26 16:47:53 +0500

    Add project files.

---
+++ TransferHomePages/Controllers/TransferHomeWebController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TransferHomePages.Controllers
+{
+    public class TransferHomeWebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+       [Route("FAQ")]
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
+        [Route("Help")]
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+
+        [Route("Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("Terms")]
+        public IActionResult Terms()
+        {
+            return View();
+        }
+    }
+}
